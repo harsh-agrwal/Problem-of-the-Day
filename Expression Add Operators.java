ede807d9bf9eb50689cae673f71df994a9614947
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-05-25 11:42:27 +0530

    Create Expression Add Operators.java

---
+++ Expression Add Operators.java
@@ -0,0 +1,31 @@
+class Solution {
+    public static ArrayList<String> addOperators(String S, int target) {
+        // code here
+        ArrayList<String> result = new ArrayList<>();
+        genExpression(S, target, 0, 0, 0,"",result);
+        return result;
+    }
+    private static void genExpression(String num, int target, int index, long value, long prevOp, String exp, ArrayList<String> result){
+        if (index == num.length()){
+            if (value == target){
+                result.add(exp);
+            }
+            return;
+        }
+        long currOp = 0;
+        for (int i = index; i < num.length(); i++){
+            if (i != index && num.charAt(index) == '0')
+                break;
+                
+            currOp = currOp* 10 +(num.charAt(i) - '0');
+            
+            if(index == 0)
+                genExpression(num, target, i+1, currOp,currOp, Long.toString(currOp), result);
+            else{
+                genExpression(num, target, i+1, value + currOp, currOp, exp + '+' + currOp, result);
+                genExpression(num, target, i+1, value - currOp, -currOp, exp + '-' + currOp, result);
+                genExpression(num, target, i+1, value - prevOp + prevOp * currOp, prevOp * currOp, exp + '*' + currOp, result);
+            }    
+        }
+    }
+}
