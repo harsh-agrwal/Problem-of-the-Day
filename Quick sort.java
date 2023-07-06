a83c995077a580e85266958327a75fac48d5c322
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-07-06 20:47:49 +0530

    Create Quick sort.java

---
+++ Quick sort.java
@@ -0,0 +1,77 @@
+Quick Sort is a Divide and Conquer algorithm. It picks an element as a pivot and partitions the given array around the picked pivot.
+Given an array arr[], its starting position is low (the index of the array) and its ending position is high(the index of the array).
+
+Note: The low and high are inclusive.
+
+Implement the partition() and quickSort() functions to sort the array.
+  Solution:
+//{ Driver Code Starts
+import java.util.*;
+class Sorting
+{
+	static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+    
+    // Driver program
+    public static void main(String args[])
+    {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T>0)
+		{
+			int n = sc.nextInt();
+			int arr[] = new int[n];
+			for(int i=0;i<n;i++)
+				arr[i] = sc.nextInt();
+
+			
+			new Solution().quickSort(arr,0,n-1);
+			printArray(arr);
+		    T--;
+		}
+} }
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to sort an array using quick sort algorithm.
+   static void quickSort(int arr[], int low, int high)
+    {
+        // code here
+        if (low < high) {
+            int partitionIndex = partition(arr, low, high);
+    
+            quickSort(arr, low, partitionIndex-1);
+            quickSort(arr, partitionIndex+1, high);
+        }
+        
+    }
+    static int partition(int arr[], int low, int high)
+    {
+        // your code here
+        int pivot = arr[high];
+        int i = (low-1);
+    
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+    
+                int swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
+            }
+        }
+    
+        int swapTemp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = swapTemp;
+    
+        return i+1;
+    } 
+}
