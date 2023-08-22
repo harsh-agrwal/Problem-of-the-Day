b2997327ce337ee5ebe3c0666fec4082527f8e20
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-08-22 21:23:31 +0530

    Create Make matrix beautiful.java

---
+++ Make matrix beautiful.java
@@ -0,0 +1,100 @@
+A beautiful matrix is a matrix in which the sum of elements in each row and column is equal. Given a square matrix of size NxN. Find the minimum number of operation(s) that are required to make the matrix beautiful. In one operation you can increment the value of any one cell by 1.
+  Solution:
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntMatrix
+{
+    public static int[][] input(BufferedReader br, int n, int m) throws IOException
+    {
+        int[][] mat = new int[n][];
+
+        for(int i = 0; i < n; i++)
+        {
+            String[] s = br.readLine().trim().split(" ");
+            mat[i] = new int[s.length];
+            for(int j = 0; j < s.length; j++)
+                mat[i][j] = Integer.parseInt(s[j]);
+        }
+
+        return mat;
+    }
+
+    public static void print(int[][] m)
+    {
+        for(var a : m)
+        {
+            for(int e : a)
+                System.out.print(e + " ");
+            System.out.println();
+        }
+    }
+
+    public static void print(ArrayList<ArrayList<Integer>> m)
+    {
+        for(var a : m)
+        {
+            for(int e : a)
+                System.out.print(e + " ");
+            System.out.println();
+        }
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int N;
+            N = Integer.parseInt(br.readLine());
+            
+            
+            int[][] matrix = IntMatrix.input(br, N, N);
+            
+            Solution obj = new Solution();
+            int res = obj.findMinOperation(N, matrix);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static int findMinOperation(int N, int[][] matrix) {
+        // code here
+         ArrayList<Integer> list = new ArrayList<>();
+    
+        for(int i=0; i<N; i++)
+        {
+            int sum = 0;
+            int sum1 = 0;
+            
+            for(int j=0; j<N; j++)
+            {
+                sum += matrix[i][j];
+                sum1 += matrix[j][i];
+            }
+            
+            list.add(sum);
+            list.add(sum1);
+        }
+        int max = Collections.max(list);
+        int ans = 0;
+        for(int i =0; i<list.size(); i++)
+        {
+            ans += max - list.get(i);
+        }
+        return ans/2;
+    }
+}
+        
